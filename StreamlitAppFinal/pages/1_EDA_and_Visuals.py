cbc6c98ea33a7c4a4c9b1c831c1b36eae21594cd
Jordyn Small <jordynsmall16@gmail.com>
2025-05-02 21:42:20 -0400

    Update Header section

--- StreamlitAppFinal/pages/1_EDA_and_Visuals.py
+++ StreamlitAppFinal/pages/1_EDA_and_Visuals.py
@@ -25,7 +25,8 @@ for col in ['store_location', 'product_category', 'product_type', 'product_detai
 coffee_palette = ['#A67B5B', '#C19A6B', '#8B6C42', '#D2B48C', '#6F4E37']
 
 # Create a page introduction section
-st.title("Explore the Data")
+st.title("☕ Behind the Beans: NYC Coffee Insights")
+st.caption("A data-rich dive into what, where, and when New Yorkers are sipping")
 st.write("Use the filters below to explore sales performance by category, time, and location.")
 
 ### filtering section 
