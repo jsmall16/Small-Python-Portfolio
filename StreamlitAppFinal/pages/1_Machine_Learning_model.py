2fc69164d18c37ef163eaf7c1b27fd48e668acd7
Jordyn Small <jordynsmall16@gmail.com>
2025-05-05 11:14:27 -0400

    Update dependancies

--- StreamlitAppFinal/pages/1_Machine_Learning_model.py
+++ StreamlitAppFinal/pages/1_Machine_Learning_model.py
@@ -1,6 +1,5 @@
 # Importing necessary libraries for data manipulation and visualization 
 import pandas as pd
-import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import streamlit as st
