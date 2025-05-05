0358929f6e21b8f2645cc94dfc37c3f1629617f7
Jordyn Small <jordynsmall16@gmail.com>
2025-05-05 09:37:01 -0400

    Edit text

--- StreamlitAppFinal/Home.py
+++ StreamlitAppFinal/Home.py
@@ -4,7 +4,7 @@ import streamlit as st
 # Homepage Title & Image
 st.title('Welcome to City Sips! ☕️')
 st.image('StreamlitAppFinal/Images/Coffeeshop.jpg')
-st.subheader('Where Data Meets Your Daily Brew 📊')
+st.subheader('Where Data Meets Your Daily Brew')
 
 # Intro Paragraph
 st.write("""
@@ -13,18 +13,17 @@ exploring how New Yorkers enjoy their coffee. Whether you're a coffee shop execu
 just curious how different New York neighborhoods enjoy their coffee, you're in the right place!
 """)
 # Purpose of App
-st.subheader("What's Brewing Inside the App 📲")
+st.subheader("What's Brewing Inside the App")
 st.write("""
 This app is designed to help business owners and coffee lovers alike:
-- Boost sales by analyzing product and sales trends  
+- Boost sales by analyzing product trends 
 - Use predictive models to forecast future sales
 The goal is to help coffee shop owners make more strategic business decisions.
 """)
 
 # Dataset description
-st.markdown("""### Coffee Trends from the Ground Up 🌱""")
-st.write("""This dataset includes over 140,000 transactions from three unique coffee shop locations across New York City:  
-    **Lower Manhattan**, **Hell’s Kitchen**, and **Astoria**.""")
+st.markdown("""### Coffee Trends from the Ground Up""")
+st.write("""This dataset includes over 140,000 transactions from three unique coffee shop locations across New York City: **Lower Manhattan**, **Hell’s Kitchen**, and **Astoria**.""")
 st.markdown("""
     Each transaction contains:
     -  A store location
@@ -33,6 +32,6 @@ st.markdown("""
     -  The quantity sold and revenue
 
     By analyzing this data, we can uncover what customers are drinking, when they’re buying it, 
-    and how each store’s performance compares. 
+    and how each store’s performance compares to one another. 
     """)
 st.markdown("### Use the sidebar on the left to navigate to **EDA & Visuals**!")
